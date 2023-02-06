ddbee9e3fcc4795dd8d89fa88830fa2a4b0db1a9
Keelan14 <116610374+Keelan14@users.noreply.github.com>
2023-02-06 11:09:12 -0600

    Create test_child.java

---
+++ test_child.java
@@ -0,0 +1,13 @@
+// print an int, string, and a bool
+
+Class Test {
+  public static void main(String [] args) {
+    int num = 5;
+    String word = "Hello";
+    boolean isTrue = false;
+    System.out.println(num);
+    System.out.println(word);
+    System.out.println(isTrue);
+    
+  }
+}
